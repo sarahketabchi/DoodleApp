50a674ffa8db6792466e0bc8298863f08bd0999e
Sarah Ketabchi <sarahketabchi@gmail.com>
2013-02-22 17:04:18 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoodleApp::Application.config.secret_token = '61f44249912826cd4d93f484a0ece0d8ef5f3b976f8d087bb13cc70af8486f00f31adfb47c673f6944695ff1b374451eae1d850ff577bde1c83c08fa05a32816'
