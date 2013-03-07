09f304d5633c6cbb04f171d85288c6804c432716
Sarah Ketabchi <sarahketabchi@gmail.com>
2013-03-06 17:51:32 -0800

    Only show public images on home page if not signed in. If signed in only show images which should be visible

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    @images = Image.all
+    #@images = Image.all
+    if (current_user == nil)
+      @images = Image.where(:privacy => 'public')
+    else
+      @images = Image.all_viewable_by(current_user)
+    end
+
     @top_image = Image.select("images.*, COUNT(*) AS favs_count")
                       .joins(:favorites)
                       .group("images.id")
--- app/models/image.rb
+++ app/models/image.rb
@@ -10,6 +10,23 @@ class Image < ActiveRecord::Base
 
   has_many :favorites
 
+  def self.all_viewable_by(user)
+    public_images = Image.where(:privacy => 'public').pluck(:id)
+
+    users_images = Image.where(:user_id => user.id).pluck(:id)
+
+    friends_friend_images = Image.joins("INNER JOIN friendships ON friendships.friend_id = images.user_id")
+          .where("friendships.user_id = 1")
+          .where("friendships.status = 'accepted'")
+          .where("images.privacy = 'friends'")
+          .pluck("images.id")
+
+    image_ids = public_images + users_images + friends_friend_images
+    image_ids.uniq!
+
+    Image.find(image_ids)
+  end
+
   def favorited?(user_id, image_id)
     if Favorite.where(:user_id => user_id, :image_id => image_id).length > 0
       return true
